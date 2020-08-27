0330dca69441a00c1cabf393405eb9d9cd00ed2d
Mike Manchee <mikemanchee@gmail.com>
2020-08-26 22:03:41 -0700

    add js functionality to Tic Tac Toe project

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function Game() {
 Game.prototype.addPlayer = function(player) {
   this.players.push(player);
 }
+
 Game.prototype.findPlayer = function() {
   for (let i = 0; i < this.players.length; i++) {
     if (this.players[i].turn === true) {
@@ -14,8 +15,11 @@ Game.prototype.findPlayer = function() {
   }
 }
 
+// 
 
 // Business Logic Player
+let game = Game();
+
 function Player(name, total, turn) {
   this.name = name,
   this.total = total,
@@ -23,36 +27,92 @@ function Player(name, total, turn) {
 }
 
 // Interface Logic
+function mark(num) {
+  let player = game.findPlayer();
+  let result = [player, num];
+  let mark;
+  if (player === 0) {
+    mark = "X";
+  } else {
+    mark = "O"
+  }
+  $("#square" + num).text(mark);
+  return result;
+}
+
+function add(array) {
+  game.players[array[0]].total += parseInt(array[1]);
+  return array[0];
+}
+
+function winner(player) {
+  let winArray = [5, 111, 222, 333, 123, 321, 50, 500];
+  let winner = false;
+  for (let i = 0; i < winArray.length; i++) {
+    if (game.players[player].total === winArray[i]) {
+      winner = true;
+    }
+  }
+}
+
+function endTurn(player) {
+
+}
+
+function actions(num) {
+  let result = mark(1);
+  let player = add(result);
+  let winner = winner(player);
+  if (winner) {
+    $("#winner-name").text(game.players[winner].name);
+    $("#winner").show();
+    $("#game-board").hide();
+  } else {
+    endTurn(player);
+  }
+}
+
 $(document).ready(function() {
   $("#start-game").submit(function() {
-    let player1Name = 
+    event.preventDefault();
+    let player1Name = $("input#player1").val();
+    let player2Name = $("input#player2").val();
+
+    $("#player1-name").text(player1Name);
+    $("#player2-name").text(player2Name);
+    $("#start-game").hide();
+    $("#game-board").show();
+
+    let player1 = new Player(player1Name, 0, true);
+    let player2 = new Player(player2Name, 0, false);
+    game.addPlayer(player1);
+    game.addPlayer(player2);
   });
   $("#square1").click(function() {
-    mark();
+    actions(1);
   });
   $("#square2").click(function() {
-    mark();
+    actions(2);
   });
   $("#square3").click(function() {
-    mark();
+    actions(3);
   });
-  $("#square4").click(function() {
-    mark();
+  $("#square10").click(function() {
+    actions(10);
   });
-  $("#square5").click(function() {
-    mark();
+  $("#square20").click(function() {
+    actions(20);
   });
-  $("#square6").click(function() {
-    mark();
+  $("#square30").click(function() {
+    actions(30);
   });
-  $("#square7").click(function() {
-    mark();
+  $("#square100").click(function() {
+    actions(100);
   });
-  $("#square8").click(function() {
-    mark();
+  $("#square200").click(function() {
+    actions(200);
   });
-  $("#square9").click(function() {
-    mark();
+  $("#square300").click(function() {
+    actions(300);
   });
-  $("#square" +  + "-text").text(mark);
 });
